f7c9a65c86542a271e009b2497fd160eeec94ec5
underscoreDev <godswill@zimmerman.team>
2022-09-16 13:20:11 +0100

    removed unnecessary dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,41 @@
-import express from 'express';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 
-(async () => {
+const app = express();
+const port = process.env.PORT || 8982;
+app.use(express.json());
 
-  // Init the Express application
-  const app = express();
+app.get("/", async (req: Request, res: Response) => {
+  res.send("try GET /filteredimage?image_url={{}}");
+});
 
-  // Set the network port
-  const port = process.env.PORT || 8982;
-  
-  // Use the body parser middleware for post requests
-  app.use(express.json());
+// Start the Server
+app.listen(port, () => {
+  console.log(`server running http://localhost:${port}`);
+  console.log(`press CTRL+C to stop server`);
+});
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+// Init the Express application
 
-  /**************************************************************************** */
+// Set the network port
 
-  //! END @TODO1
-  
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+// Use the body parser middleware for post requests
 
-  // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
-})();
+// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+// GET /filteredimage?image_url={{URL}}
+// endpoint to filter an image from a public url.
+// IT SHOULD
+//    1. validate the image_url query
+//    2. call filterImageFromURL(image_url) to filter the image
+//    3. send the resulting file in the response
+//    4. deletes any files on the server on finish of the response
+// QUERY PARAMATERS
+//    image_url: URL of a publicly accessible image
+// RETURNS
+//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+/**************************************************************************** */
+
+//! END @TODO1
+
+// Root Endpoint
+// Displays a simple message to the user
