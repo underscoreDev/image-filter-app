d97f9cf2bae58f2a2946423c8f2c885a52c41d48
underscoreDev <godswill@zimmerman.team>
2022-09-16 22:40:17 +0100

    final commit

--- dist/routes/image.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const imageFilter_controller_1 = require("./../controllers/imageFilter.controller");
-const catchAsyncError_middleware_1 = require("./../middlewares/catchAsyncError.middleware");
-const imageRouter = (0, express_1.Router)();
-imageRouter.route("/filteredimage").get((0, catchAsyncError_middleware_1.catchAsync)(imageFilter_controller_1.filterImage));
-exports.default = imageRouter;
-//# sourceMappingURL=image.route.js.map
