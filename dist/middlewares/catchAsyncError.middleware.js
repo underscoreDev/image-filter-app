585440622efbb5498fbe13ef6dae7f6d45d37dfb
Godswill Edet <97456640+underscoreDev@users.noreply.github.com>
2022-09-16 22:28:46 +0100

    Delete dist directory

--- dist/middlewares/catchAsyncError.middleware.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsync = void 0;
-const catchAsync = (fn) => (req, res, next) => fn(req, res, next).catch(next);
-exports.catchAsync = catchAsync;
-//# sourceMappingURL=catchAsyncError.middleware.js.map
